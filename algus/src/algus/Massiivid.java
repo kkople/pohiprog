d7281417a2ab56ea8ba41d87f38bda07c742a878
Kaspar <>
2018-02-01 11:15:47 +0200

    Programm

--- algus/src/algus/Massiivid.java
+++ algus/src/algus/Massiivid.java
@@ -3,6 +3,26 @@ package algus;
 import java.util.Arrays;
 
 public class Massiivid {
+    //Koosta funktsioon, mis tagastab kaheelemendilise massiivi,
+    //mille esimeseks elemendiks on etteantud arvudest suurim
+    //ning teiseks vähim
+    
+    public static int[] piirid(int[] arvud){
+        int[] vastus=new int[2];
+        if (arvud==null){
+            throw new RunTimeException("parameetri väärtus null");
+        }
+        if(arvud.length==0){
+            throw new RunTimeException("parameetriks tühi massiiv");
+        }
+        vastus[0]=vastus[1]=arvud[0]; //esimese arvu omistab vastuse mõlemale
+        for(int arv: arvud){
+            if(arv<vastus[0]){vastus[0]=arv;}
+            if(arv>vastus[1]){vastus[1]=arv;}
+        }
+        return vastus;
+    }
+    
     public static int summa(int[] arvud){
         int summa=0;
         for(int i=0; i<arvud.length; i++){
@@ -10,8 +30,15 @@ public class Massiivid {
         }
         return summa;
     }
+    public static double keskmine(int[] arvud){
+        return summa(arvud)*1.0/arvud.length;
+    }
     public static void main(String[] arg) {
         int[] pikkused={176, 163, 158, 171, 169};
+        int[] t=piirid(pikkused);
+        
+        System.out.println(summa(pikkused));
+        System.out.println(keskmine(pikkused));
         System.out.println("Kokku: "+pikkused.length);
         System.out.println(pikkused[0]+" "+pikkused[pikkused.length-1]);
         for(int i=0; i<pikkused.length; i++){
