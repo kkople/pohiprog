02d11fb1543b9ddefebbb08fefd87ad120d85c2b
if17 <if17@A406-23.INFORMAATIKA>
2018-02-01 09:34:40 +0200

    Programm

---
+++ algus/src/algus/Algus.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algus;
+
+/**
+ *Esimene katsetus. Muutujad, valik, massiiv
+ * @author if17
+ */
+public class Algus {
+
+    /**
+     * Programmi algus
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, Web");
+        int temperatuur=-2;
+        if(temperatuur>0){System.out.println("sulab");}
+        else(System.out.println("Jäätab");}
+    }   
+    
+    
+}
